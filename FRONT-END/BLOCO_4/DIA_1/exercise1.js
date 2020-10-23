8fc22ac0d9b14b09b41fd4c0e2eb721ba816522e
Patrícia <silva.patricia.v@gmail.com>
2020-10-23 18:45:30 -0300

    exercise 1 completed

---
+++ FRONT-END/BLOCO_4/DIA_1/exercise1.js
@@ -0,0 +1,11 @@
+let a = 20;
+let b = 10;
+let adicao
+let subtracao;
+adicao = a + b;
+subtracao = a - b;
+multiplicacao = a * b;
+divisao = a / b;
+modulo = a % b;
+
+console.log(adicao, subtracao, multiplicacao,divisao, modulo);
