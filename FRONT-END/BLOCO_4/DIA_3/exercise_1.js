673c993b33ed6fdb7d8b0a00076120146f15737f
Patrícia <silva.patricia.v@gmail.com>
2020-11-02 19:25:06 -0300

    Cria diretório e arquivos do Bloco 5

---
+++ FRONT-END/BLOCO_4/DIA_3/exercise_1.js
@@ -0,0 +1,11 @@
+let n = 5;
+let line;
+let symbol = '*';
+let output = '';
+
+for (lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+}
+for (lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  console.log(inputLine);
+}
